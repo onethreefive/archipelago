f43459dc019719bdca3723f9453c6557851ef097
PaulS <psirov@gmail.com>
2013-10-18 00:35:26 -0700

    created boilerplate SDL code

--- archipelago.cpp
+++ archipelago.cpp
@@ -6,6 +6,35 @@
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+
+	if (SDL_Init(SDL_INIT_EVERYTHING) == -1){
+        std::cout << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+	SDL_Window *window = nullptr;
+	window = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+	if (window == nullptr){
+        std::cout << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+	SDL_Renderer *renderer = nullptr;
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (renderer == nullptr){
+        std::cout << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+	SDL_Delay(2000);
+
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
+
+	SDL_Quit();
+
 	return 0;
 }
 
--- stdafx.h
+++ stdafx.h
@@ -10,6 +10,9 @@
 #include <stdio.h>
 #include <tchar.h>
 
+#include <iostream>
+#include <SDL.h>
+
 
 
 // TODO: reference additional headers your program requires here
